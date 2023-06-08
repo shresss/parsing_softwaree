f4cb592c1dcaa2e93f06744eb29a94845ab81381
Usha Venkat <ushavenkat@alum.calarts.edu>
2023-06-08 02:19:39 -0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -14,7 +14,11 @@ for page in range(number_of_pages):
 
 tables = tabula.read_pdf('path/to/pdf_file.pdf', pages='all')
 """parses tabular data with tabula"""
-
+column_widths = [max(len(item) for item in column) for column in zip(*tables)]
+formatted_table = ''
+for row in tables:
+    formatted_row = ' | '.join(f'{item:^{width}}' for item, width in zip(row, column_widths))
+    formatted_table += f'{formatted_row}\n'
 for table in tables:
     print(table)
 """accesses parsed data , will print each as a Pandas data frame
